f5dcf8f26f5872d0699ef5a11b73fa62d42e21c3
VladisssLuv <vladissslav19@gmail.com>
2024-05-01 18:49:54 +0300

    upd: LocalizedStrings, add new ru and en strings

--- core/presentation/src/main/java/inc/generics/presentation/theme/localization/LocalizedStrings.kt
+++ core/presentation/src/main/java/inc/generics/presentation/theme/localization/LocalizedStrings.kt
@@ -2,8 +2,17 @@ package inc.generics.presentation.theme.localization
 
 internal val russianStrings = hashMapOf(
     Pair("duet", "Дуэт"),
+    Pair("authByVk", "Войти через ВК"),
+    Pair("authText", "Войдите или зарегестрируйтесь.\n" +
+            "Используйте любой из предложенных\n" +
+            "ниже способов авторизации"
+    ),
 )
 
 internal val englishStrings = hashMapOf(
     Pair("duet", "Duet"),
+    Pair("authByVk", "Login via VK"),
+    Pair("authText", "Login or register.\n" +
+            "Use any of the suggested\n" +
+            "Below is the login method"),
 )
