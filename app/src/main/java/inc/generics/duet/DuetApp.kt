0cfcc0fa2f3f58a942aa4e6126543eb29d91859c
VladisssLuv <vladissslav19@gmail.com>
2024-05-01 18:54:25 +0300

    upd: DuetApp, add start di

--- app/src/main/java/inc/generics/duet/DuetApp.kt
+++ app/src/main/java/inc/generics/duet/DuetApp.kt
@@ -1,6 +1,22 @@
 package inc.generics.duet
 
 import android.app.Application
+import inc.generics.duet.glue.authorization.di.authorizationFeatureModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class DuetApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@DuetApp)
+            modules(
+                authorizationFeatureModule
+            )
+        }
+    }
 }
