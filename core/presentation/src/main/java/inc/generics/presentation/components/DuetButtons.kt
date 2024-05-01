f827301ee73e2e39f838241f3591be32b8bdd25f
VladisssLuv <vladissslav19@gmail.com>
2024-05-01 18:48:53 +0300

    upd: DuetButtons, default modifier for default buttons

--- core/presentation/src/main/java/inc/generics/presentation/components/DuetButtons.kt
+++ core/presentation/src/main/java/inc/generics/presentation/components/DuetButtons.kt
@@ -3,7 +3,6 @@ package inc.generics.presentation.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FilledTonalButton
@@ -12,8 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import inc.generics.presentation.theme.DuetTheme
 
@@ -60,7 +57,7 @@ fun FilledTonalButtonDuet(
 fun DefaultOutlinedButtonDuet(
     onClick: () -> Unit,
     text: String,
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
     OutlinedButtonDuet(
         onClick = onClick,
@@ -77,8 +74,8 @@ fun DefaultOutlinedButtonDuet(
 fun DefaultFilledTonalButtonDuet(
     onClick: () -> Unit,
     text: String,
+    modifier: Modifier = Modifier,
     color: Color = DuetTheme.colors.secondColor,
-    modifier: Modifier
 ) {
     FilledTonalButtonDuet(
         onClick = onClick,
