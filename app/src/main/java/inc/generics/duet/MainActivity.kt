fa6e6d4e289efdfa0bf1471f3cd6fea3090a73e7
VladisssLuv <vladissslav19@gmail.com>
2024-05-03 18:11:13 +0300

    upd: MainActivity.kt, add localization observer

--- app/src/main/java/inc/generics/duet/MainActivity.kt
+++ app/src/main/java/inc/generics/duet/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.compose.rememberNavController
 import inc.generics.duet.navigation.SetupMainNavGraph
 import inc.generics.presentation.theme.AppDuetTheme
+import inc.generics.presentation.theme.appDuetThemeViewModel
+import inc.generics.presentation.theme.localization.Language
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AppDuetTheme {
+            val language by appDuetThemeViewModel().localization.observeAsState(
+                Language.En()
+            )
+            AppDuetTheme(localization = language) {
                 Main()
             }
         }
