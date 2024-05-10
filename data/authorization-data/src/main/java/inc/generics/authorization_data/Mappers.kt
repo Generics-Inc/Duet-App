e787fe359e347215ed91fbbceb738649b4fc29db
VladisssLuv <vladissslav19@gmail.com>
2024-05-10 19:44:03 +0300

    fix: Mappers (auth) to "internal"

--- data/authorization-data/src/main/java/inc/generics/authorization_data/Mappers.kt
+++ data/authorization-data/src/main/java/inc/generics/authorization_data/Mappers.kt
@@ -3,7 +3,7 @@ package inc.generics.authorization_data
 import inc.generics.authorization_data.util.DeviceInf
 import inc.generics.duet_api.models.auth.DeviceInfDto
 
-fun DeviceInf.toDto(): DeviceInfDto = DeviceInfDto(
+internal fun DeviceInf.toDto(): DeviceInfDto = DeviceInfDto(
     uuid = uuid,
     name = name,
     os = os
