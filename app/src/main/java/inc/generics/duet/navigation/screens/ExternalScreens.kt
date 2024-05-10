dbaafa33efc2d6db0864951de19168189345896a
VladisssLuv <vladissslav19@gmail.com>
2024-05-10 18:46:17 +0300

    fix: ExternalScreens, add dataKey

--- app/src/main/java/inc/generics/duet/navigation/screens/ExternalScreens.kt
+++ app/src/main/java/inc/generics/duet/navigation/screens/ExternalScreens.kt
@@ -1,7 +1,7 @@
 package inc.generics.duet.navigation.screens
 
-sealed class ExternalScreens(val route: String) {
-    data object Main : ExternalScreens(route = "externalMainScreen")
-    data object NoActiveGroup : ExternalScreens(route = "noActiveGroupScreen")
-    data object Authorization : ExternalScreens(route = "authorizationScreen")
+sealed class ExternalScreens(val route: String, val dataKey: String) {
+    data object Main : ExternalScreens(route = "externalMainScreen", "")
+    data object NoActiveGroup : ExternalScreens(route = "noActiveGroupScreen", "dataNoActiveGroup")
+    data object Authorization : ExternalScreens(route = "authorizationScreen", "")
 }
