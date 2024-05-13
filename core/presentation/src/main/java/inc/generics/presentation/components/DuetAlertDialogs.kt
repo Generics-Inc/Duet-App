1d27fa40e2a961f1cd73f29c067a55f4624151dc
VladisssLuv <vladissslav19@gmail.com>
2024-05-13 10:51:48 +0300

    feat: add DuetAlertDialogs.kt, errorDialog

---
+++ core/presentation/src/main/java/inc/generics/presentation/components/DuetAlertDialogs.kt
@@ -0,0 +1,49 @@
+package inc.generics.presentation.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import inc.generics.presentation.R
+import inc.generics.presentation.theme.DuetTheme
+
+@Composable
+fun DuetAlertDialogError(
+    messageText: String,
+    onClose: () -> Unit
+) {
+    AlertDialog(
+        icon = {
+            Icon(
+                painterResource(id = R.drawable.ic_alert),
+                contentDescription = "error icon",
+                tint = DuetTheme.colors.errorColor
+            )
+        },
+        title = {
+            Text(
+                text = DuetTheme.localization.getString("dialogErrorTitle"),
+                style = defaultDialogTextStyleDuet().copy(
+                    color = DuetTheme.colors.errorColor,
+                    fontSize = 18.sp
+                )
+            )
+        },
+        text = {
+            Text(
+                text = messageText,
+                style = defaultDialogTextStyleDuet().copy(color = DuetTheme.colors.errorColor)
+            )
+        },
+        onDismissRequest = { onClose() },
+        confirmButton = {
+            DefaultDialogOutlinedButtonDuet(
+                onClick = { onClose() },
+                text = DuetTheme.localization.getString("okOnError")
+            )
+        },
+        containerColor = DuetTheme.colors.backgroundColor
+    )
+}
