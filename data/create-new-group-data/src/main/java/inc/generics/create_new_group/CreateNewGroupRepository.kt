d136f965dc8121d69fa6dd372ca3f43b670ba115
VladisssLuv <vladissslav19@gmail.com>
2024-05-13 10:52:59 +0300

    fix: CreateNewGroupRepository, just refactor

--- data/create-new-group-data/src/main/java/inc/generics/create_new_group/CreateNewGroupRepository.kt
+++ data/create-new-group-data/src/main/java/inc/generics/create_new_group/CreateNewGroupRepository.kt
@@ -1,39 +1,28 @@
 package inc.generics.create_new_group
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import inc.generics.create_new_group.models.Group
 import inc.generics.duet_api.api.DuetApi
 import inc.generics.duet_api.util.sendRequestToCreateGroup
 import java.io.File
-import java.io.FileOutputStream
 import java.util.Date
 
 class CreateNewGroupRepository(private val api: DuetApi, private val context: Context) {
     suspend fun create(group: Group): Boolean {
-        val file = File(getRealPathFromURI(group.fileUri!!))
-
-        Log.d("File", file.toString())
-        Log.d("File", file.isFile.toString())
-        Log.d("File", file.absoluteFile.canRead().toString())
-        Log.d("File", file.absoluteFile.toString())
         val result = sendRequestToCreateGroup(
             api = api,
             name = group.name,
             description = "this group is so cute!",
             relationStartedAt = Date(),
-            file = if (group.fileUri?.path.isNullOrEmpty()) null else file
+            file = if (group.fileUri?.path.isNullOrEmpty()) null else File(getRealPathFromURI(group.fileUri!!))
         )
 
         return result.isSuccess
     }
     private fun getRealPathFromURI(uri: Uri): String {
-        var result = ""
+        val result: String
         val cursor = context.contentResolver.query(uri, null, null, null, null)
         if (cursor == null) {
             result = uri.path ?: ""
