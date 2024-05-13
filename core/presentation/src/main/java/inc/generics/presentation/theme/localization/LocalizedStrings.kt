4aefe8b34c59d0cb72316a60002e3ee457cbc260
VladisssLuv <vladissslav19@gmail.com>
2024-05-13 14:50:06 +0300

    upd: and new localized strings

--- core/presentation/src/main/java/inc/generics/presentation/theme/localization/LocalizedStrings.kt
+++ core/presentation/src/main/java/inc/generics/presentation/theme/localization/LocalizedStrings.kt
@@ -2,6 +2,7 @@ package inc.generics.presentation.theme.localization
 
 internal val russianStrings = hashMapOf(
     Pair("duet", "Дуэт"),
+    Pair("yes", "Да"),
     Pair("authByVk", "Войти через ВК"),
     Pair("authText", "Войдите или зарегестрируйтесь.\n" +
             "Используйте любой из предложенных\n" +
@@ -19,11 +20,23 @@ internal val russianStrings = hashMapOf(
     Pair("noActiveGroup", "У вас нет\n" +
             "активной группы"),
     Pair("intoToGroup", "Присоединиться"),
-    Pair("basket", "Корзина")
+    Pair("basket", "Корзина"),
+
+    Pair("titleRequestDialog", "Точно?"),
+    Pair("newCode", "Новый код"),
+    Pair("saveArchive", "Скачать архив"),
+    Pair("partnerDeleteGroup", "Ваш партнер \n" +
+            "вышел из группы"),
+    Pair("getOut", "Выйти"),
+    Pair("partnerLeave", "Ваш партнер \n" +
+            "поместил группу\n" +
+            "в корзину"),
+    Pair("requestDeleteGroup", "Вы уверены, что хотети отправить группу в корзину?"),
 )
 
 internal val englishStrings = hashMapOf(
     Pair("duet", "Duet"),
+    Pair("yes", "Yes"),
     Pair("authByVk", "Login via VK"),
     Pair("authText", "Login or register.\n" +
             "Use any of the suggested\n" +
@@ -40,5 +53,16 @@ internal val englishStrings = hashMapOf(
     Pair("noActiveGroup", "You don't have\n" +
             "an active group"),
     Pair("intoToGroup", "Join"),
-    Pair("basket", "Basket")
+    Pair("basket", "Basket"),
+
+    Pair("titleRequestDialog", "Are you sure?"),
+    Pair("newCode", "New inviteCode"),
+    Pair("saveArchive", "Download archive"),
+    Pair("partnerLeave", "Your partner \n" +
+            "dropped out"),
+    Pair("getOut", "Get out"),
+    Pair("partnerDeleteGroup", "Your partner \n" +
+            "has placed the group\n" +
+            "in the basket"),
+    Pair("requestDeleteGroup", "Are you sure you want to send the group to the basket?"),
 )
