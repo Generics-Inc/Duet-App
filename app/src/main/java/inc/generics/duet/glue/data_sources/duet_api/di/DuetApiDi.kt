dfe05a1cb33462136e299d52801187f65285245a
VladisssLuv <vladissslav19@gmail.com>
2024-05-15 11:32:53 +0300

    fix: base_url in strings.xml

--- app/src/main/java/inc/generics/duet/glue/data_sources/duet_api/di/DuetApiDi.kt
+++ app/src/main/java/inc/generics/duet/glue/data_sources/duet_api/di/DuetApiDi.kt
@@ -1,13 +1,15 @@
 package inc.generics.duet.glue.data_sources.duet_api.di
 
+import inc.generics.duet.R
 import inc.generics.duet.glue.data_sources.duet_api.TokenProviderImpl
 import inc.generics.duet_api.duetApi
 import inc.generics.duet_api.util.TokenProvider
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val duetApiModule = module {
     singleOf(::TokenProviderImpl) { bind<TokenProvider>() }
-    single { duetApi("https://duet.lorexiq.ru/api/", get()) }
+    single { duetApi(androidApplication().getString(R.string.base_url), get()) }
 }
