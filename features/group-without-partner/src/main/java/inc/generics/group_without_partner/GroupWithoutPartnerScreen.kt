ba15033a50b7a79513d56001566f363dcf9d8464
VladisssLuv <vladissslav19@gmail.com>
2024-05-19 18:29:26 +0300

    clear imports GroupWithoutPartnerScreen ((

--- features/group-without-partner/src/main/java/inc/generics/group_without_partner/GroupWithoutPartnerScreen.kt
+++ features/group-without-partner/src/main/java/inc/generics/group_without_partner/GroupWithoutPartnerScreen.kt
@@ -17,9 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,15 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import inc.generics.group_without_partner.routing.GroupWithoutPartnerRouting
 import inc.generics.presentation.R
 import inc.generics.presentation.components.DefaultFilledTonalButtonDuet
