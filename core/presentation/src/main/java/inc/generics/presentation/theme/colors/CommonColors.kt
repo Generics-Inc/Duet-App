ea8e2f5ae5d0be5ee104b300c51dfc0425a4c95d
VladisssLuv <vladissslav19@gmail.com>
2024-05-21 00:11:02 +0300

    feat: add common colors for vk

---
+++ core/presentation/src/main/java/inc/generics/presentation/theme/colors/CommonColors.kt
@@ -0,0 +1,9 @@
+package inc.generics.presentation.theme.colors
+
+import androidx.compose.ui.graphics.Color
+
+data class VkColors(
+    val mainColor: Color = Color(0xFF0077FF)
+)
+
+val vkColors = VkColors()
