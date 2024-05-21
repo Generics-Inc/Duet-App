9a81d361c2b38f3f97bac25de46690512b50332a
VladisssLuv <vladissslav19@gmail.com>
2024-05-21 23:59:04 +0300

    upd: fix common ui

--- core/presentation/src/main/java/inc/generics/presentation/components/DuetAsyncImage.kt
+++ core/presentation/src/main/java/inc/generics/presentation/components/DuetAsyncImage.kt
@@ -2,10 +2,12 @@ package inc.generics.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,9 +26,10 @@ fun DuetAsyncImage(
     roundedDp: Dp = 8.dp,
     painterIconIfNotImg: Painter,
     imgUrl: String?,
+    size: Dp = 80.dp,
     modifier: Modifier = Modifier
-        .size(80.dp)
-        .padding(top = 14.dp)
+        .padding(14.dp)
+        .size(size)
         .background(
             color = DuetTheme.colors.backgroundColor,
             shape = RoundedCornerShape(roundedDp)
@@ -35,20 +38,27 @@ fun DuetAsyncImage(
     Box(
         modifier = modifier
     ) {
-        if (imgUrl == null) {
-            Icon(
-                painter = painterIconIfNotImg,
-                tint = DuetTheme.colors.mainColor,
-                contentDescription = "icon load image",
-                modifier = Modifier.align(Alignment.Center)
-            )
-        } else {
-            AsyncImage(
-                model = LocalContext.current.getString(R.string.base_url) + imgUrl,
-                contentDescription = "",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.clip(RoundedCornerShape(roundedDp))
-            )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = DuetTheme.colors.backgroundColor,
+            shadowElevation = 8.dp,
+            shape = RoundedCornerShape(roundedDp)
+        ) {
+            if (imgUrl == null) {
+                Icon(
+                    painter = painterIconIfNotImg,
+                    tint = DuetTheme.colors.mainColor,
+                    contentDescription = "icon load image",
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            } else {
+                AsyncImage(
+                    model = LocalContext.current.getString(R.string.base_url) + imgUrl,
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.clip(RoundedCornerShape(roundedDp))
+                )
+            }
         }
     }
 }
