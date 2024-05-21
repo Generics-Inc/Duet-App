f9ebc37f42ef516427772f229cc48487f51ba8ad
VladisssLuv <vladissslav19@gmail.com>
2024-05-22 00:01:11 +0300

    fix: Mappers in requests-data

--- data/requests-data/src/main/java/inc/generics/requests_data/Mappers.kt
+++ data/requests-data/src/main/java/inc/generics/requests_data/Mappers.kt
@@ -3,7 +3,7 @@ package inc.generics.requests_data
 import inc.generics.duet_api.models.groups.RequestToGroup
 import inc.generics.requests_data.models.Request
 
-fun RequestToGroup.toRequest(): Request = Request(
+internal fun RequestToGroup.toRequest(): Request = Request(
     id = this.id,
     profileId = this.profileId,
     firstName = this.firstName,
