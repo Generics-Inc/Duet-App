f169b1449a6e3734fa94cdb7605ecc5781ac0282
VladisssLuv <vladissslav19@gmail.com>
2024-06-03 18:35:35 +0300

    upd: StatusGroupLeftByPartner impl to Parcelable

--- features/group-left-by-partner/src/main/java/inc/generics/group_left_by_partner/models/StatusGroupLeftByPartner.kt
+++ features/group-left-by-partner/src/main/java/inc/generics/group_left_by_partner/models/StatusGroupLeftByPartner.kt
@@ -1,6 +1,10 @@
 package inc.generics.group_left_by_partner.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class StatusGroupLeftByPartner(
     val isMainInGroup: Boolean,
     val isPartnerDeleteGroup: Boolean
-)
+) : Parcelable
