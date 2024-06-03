7065f6e8f12e180a9b72620bb740b00eb0f9051c
VladisssLuv <vladissslav19@gmail.com>
2024-06-03 18:33:25 +0300

    upd: Build_gradle, add "kotlin-parcelize"

--- features/group-left-by-partner/build.gradle.kts
+++ features/group-left-by-partner/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.jetpack.compose.compiler)
+    id("kotlin-parcelize")
 }
 
 android {
