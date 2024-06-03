d93d83ecb9cebccc1a35dad146e3fd98854aa6e9
VladisssLuv <vladissslav19@gmail.com>
2024-06-03 18:38:32 +0300

    upd: fix NoActiveGroupScreen, delete imports

--- features/no-active-group/src/main/java/inc/generics/no_active_group/NoActiveGroupScreen.kt
+++ features/no-active-group/src/main/java/inc/generics/no_active_group/NoActiveGroupScreen.kt
@@ -4,22 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import inc.generics.no_active_group.models.NoActiveGroupUiData
 import inc.generics.no_active_group.routing.NoActiveGroupRouting
-import inc.generics.presentation.R
 import inc.generics.presentation.components.DefaultTopAppBarDuet
 import inc.generics.presentation.theme.DuetTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoActiveGroupScreen(
     routing: NoActiveGroupRouting,
