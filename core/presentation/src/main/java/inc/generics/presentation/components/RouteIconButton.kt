450e5f6dfc5b16e161aa804e93d1f618fdc7e15e
VladisssLuv <vladissslav19@gmail.com>
2024-06-05 22:25:18 +0300

    upd: add RouteIconButtonAnimated

--- core/presentation/src/main/java/inc/generics/presentation/components/RouteIconButton.kt
+++ core/presentation/src/main/java/inc/generics/presentation/components/RouteIconButton.kt
@@ -1,5 +1,6 @@
 package inc.generics.presentation.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -14,8 +15,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -33,7 +39,7 @@ data class RouteButtonModel(
 @Composable
 fun RouteIconButton(
     model: RouteButtonModel,
-    onClick: () -> Unit = {}
+    onClick: () -> Unit = {},
 ) {
     Row(verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
@@ -81,6 +87,69 @@ fun RouteIconButton(
             modifier = Modifier
                 .padding(end = 16.dp)
                 .size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun RouteIconButtonAnimated(
+    model: RouteButtonModel,
+    onClick: () -> Unit = {},
+) {
+    var isClick by remember {
+        mutableStateOf(false)
+    }
+
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(78.dp)
+            .clickable {
+                onClick()
+                isClick = !isClick
+            }
+    ) {
+        if (model.icon != null) {
+            Image(modifier = Modifier
+                .padding(start = 16.dp)
+                .size(24.dp)
+                .background(color = Color.Gray, shape = RoundedCornerShape(8.dp)),
+                painter = painterResource(model.icon),
+                contentDescription = ""
+            )
+        } else {
+            Box(modifier = Modifier
+                .padding(start = 16.dp)
+                .size(24.dp)
+                .background(color = DuetTheme.colors.mainColor, shape = RoundedCornerShape(8.dp))
+            )
+        }
+
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(start = 24.dp, end = 24.dp)
+        ) {
+            Text(text = model.title, style = defaultDialogTextStyleDuet())
+            model.additionalText?.let {
+                Text(text = it,
+                    style = TextStyle(
+                        color = DuetTheme.colors.textColor.copy(alpha = 0.5f),
+                        fontSize = 12.sp
+                    )
+                )
+            }
+
+        }
+
+        Icon(painter = painterResource(id = R.drawable.ic_arrow_forward),
+            tint = DuetTheme.colors.thirdColor,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .size(24.dp).rotate(animateFloatAsState(
+                    if(isClick) 90f else 0f,
+                    label = "anim"
+                ).value)
         )
 
     }
