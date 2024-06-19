5a5c2931efe61dbbe60285ccaf5486571c4b249e
VladisssLuv <vladissslav19@gmail.com>
2024-06-19 15:50:41 +0300

    upd: Group-feature, logic for vm

--- features/group/src/main/java/inc/generics/group/vm/GroupViewModel.kt
+++ features/group/src/main/java/inc/generics/group/vm/GroupViewModel.kt
@@ -14,7 +14,31 @@ class GroupViewModel(
     private val _dataScreen: MutableLiveData<Group?> = MutableLiveData(null)
     val dataScreen: LiveData<Group?> = _dataScreen
 
+    private val _screenStatus = MutableLiveData(ScreenStatus.NONE)
+    val screenStatus: LiveData<ScreenStatus> = _screenStatus
+
     fun getGroup() = viewModelScope.launch {
-        _dataScreen.value = repository.getCurrentGroup()
+        repository.getCurrentGroup()?.let {
+            _dataScreen.value = it
+        } ?: onError()
     }
+
+    fun leaveGroup() = viewModelScope.launch {
+        repository.deleteGroup().let {
+            if (it)
+                _screenStatus.value = ScreenStatus.ON_LEAVE
+            else
+                onError()
+        }
+    }
+
+    private fun onError() {
+        _screenStatus.value = ScreenStatus.NETWORK_ERROR
+    }
+}
+
+enum class ScreenStatus {
+    NONE,
+    NETWORK_ERROR,
+    ON_LEAVE
 }
