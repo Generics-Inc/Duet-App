855ab1093b9fcbb9fc4beee4e084e45bff3c3d41
VladisssLuv <vladissslav19@gmail.com>
2024-06-19 15:51:53 +0300

    upd: Duet-api, add one todo

--- data-sources/duet-api/src/main/java/inc/generics/duet_api/util/StringToLocalDateTime.kt
+++ data-sources/duet-api/src/main/java/inc/generics/duet_api/util/StringToLocalDateTime.kt
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
+//todo: удалить так как используется .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") в Gson
 internal fun stringToLocalDateTime(dateTime: String): LocalDateTime? =
     try {
         LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_DATE_TIME)
