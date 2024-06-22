b8a6af8dfb82df4a63ccfc0089533c5053142612
VladisssLuv <vladissslav19@gmail.com>
2024-06-22 18:46:19 +0300

    upd: Movie for ui

--- features/movie/src/main/java/inc/generics/movie/models/Movie.kt
+++ features/movie/src/main/java/inc/generics/movie/models/Movie.kt
@@ -1,7 +1,24 @@
 package inc.generics.movie.models
 
 data class Movie(
+    val createdMovie: CreatedMovie?,
+    val creator: Creator,
+    val taskCreate: TaskCreate?,
+)
+
+data class CreatedMovie(
     val name: String,
     val description: String,
-    val isWatched: Boolean
+    val isWatched: Boolean,
+    val photoUrl: String?
+)
+
+data class TaskCreate(
+    val name: String,
+    val isError: Boolean
+)
+
+data class Creator(
+    val name: String,
+    val photoUrl: String?
 )
